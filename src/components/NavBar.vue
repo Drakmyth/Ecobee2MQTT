89e6d249294a0fc004b8443d8b82e91bdf972a0c
Drakmyth <shaun.hamman@gmail.com>
2022-07-24 10:43:28 -0700

    Add NavBar componenet

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <nav class="navbar">
+        <span class="navbar-title">Ecobee2MQTT</span>
+        <ul>
+            <li>
+                <RouterLink to="/">Devices</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/settings">Settings</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style>
+.navbar {
+    display: flex;
+    flex-direction: row;
+}
+
+.navbar .navbar-title {
+    align-self: center;
+}
+
+.navbar ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    --nav-gap: 12px;
+    margin: 0 var(--nav-gap);
+    padding: 0;
+    column-gap: var(--nav-gap);
+}
+
+.navbar a {
+    padding: 0.5rem;
+    display: block;
+}
+</style>
