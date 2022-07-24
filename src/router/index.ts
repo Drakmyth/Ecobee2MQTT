460f776fe2579158be9fd6b618b2d00aa5f22084
Drakmyth <shaun.hamman@gmail.com>
2022-07-24 11:28:30 -0700

    Make indentation consistent

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,19 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: () => import('../views/SettingsView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/settings',
+            name: 'settings',
+            component: () => import('../views/SettingsView.vue')
+        }
+    ]
 })
 
 export default router
