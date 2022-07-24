fb1548e4ea6026af8ccb85ac6c0c532598674a40
Drakmyth <shaun.hamman@gmail.com>
2022-07-24 13:27:05 -0700

    Add placeholder Ecobee configuration form

--- src/components/EcobeeConfiguration.vue
+++ src/components/EcobeeConfiguration.vue
@@ -4,6 +4,16 @@
 <template>
     <div class="ecobee-config">
         <h2>Ecobee Configuration</h2>
+        <form>
+            <label for="apikey">API Key:</label>
+            <input type="text" id="apikey" name="apikey" /><br />
+            <button>Request PIN</button><br />
+            <span>PIN: </span>
+            <span>ABC123</span><br />
+            <span>Time Remaining: </span>
+            <span>9:32</span><br />
+            <button>Complete Authorization</button>
+        </form>
     </div>
 </template>
 
