80530cac456f59f56e28fc92f139fb5c26badcfb
Drakmyth <shaun.hamman@gmail.com>
2022-07-25 06:10:16 -0700

    Configure frontend dev server to proxy to backend server instance

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
-    plugins: [vue(), vueJsx()]
+    plugins: [vue(), vueJsx()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true
+            }
+        }
+    }
 });
