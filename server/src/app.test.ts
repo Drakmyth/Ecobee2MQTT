a8d7969d158b75ee134e75210c68d2f85d7eabc8
Drakmyth <shaun.hamman@gmail.com>
2022-07-29 07:03:21 -0700

    Convert server jest configuration to typescript

--- server/src/app.test.ts
+++ server/src/app.test.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import request from "supertest";
 
 describe("GET /api", () => {
-    it("Hello Request", async () => {
+    test("Hello Request", async () => {
         const result = await request(app).get("/api");
         expect(JSON.parse(result.text)).toEqual({message: "Hello from server!"});
         expect(result.statusCode).toEqual(200);
