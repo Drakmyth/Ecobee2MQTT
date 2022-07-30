5ade59e37e75d979ed7fc50b4bd3606bc679cce3
Drakmyth <shaun.hamman@gmail.com>
2022-07-30 07:47:13 -0700

    Configure code coverage

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -12,6 +12,12 @@ const config: Config.InitialOptions = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
     },
-    moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue']
+    moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue'],
+    collectCoverageFrom: [
+        'src/**/*.{ts,vue}',
+        '!src/shims-vue.d.ts',
+        '!src/main.ts',
+        '!src/router/index.ts' // makes coverage crash, see https://github.com/facebook/jest/issues/13007
+    ]
 };
 export default config;
