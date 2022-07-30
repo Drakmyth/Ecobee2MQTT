9a0668abe54b998d142427242a8fb00ef1ed93a8
Drakmyth <shaun.hamman@gmail.com>
2022-07-30 09:07:52 -0700

    Fix formatting

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 describe("GET /api", () => {
     test("Hello Request", async () => {
         const result = await request(app).get("/api");
-        expect(JSON.parse(result.text)).toEqual({message: "Hello from server!"});
+        expect(JSON.parse(result.text)).toEqual({ message: "Hello from server!" });
         expect(result.statusCode).toEqual(200);
     });
 });
