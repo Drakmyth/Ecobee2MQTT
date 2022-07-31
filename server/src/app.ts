aba8de3dc65df86f91cb1f402bf2c1cb81c4b077
Drakmyth <shaun.hamman@gmail.com>
2022-07-30 22:12:56 -0700

    Add proxy authenticate endpoint to get around Ecobee API not implementing CORS properly

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,26 @@
 import express, { RequestHandler } from 'express';
 import path from 'path';
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 
-const apiHandler: RequestHandler = (req, res) => {
-    res.json({ message: 'Hello from server!' });
+app.use(cors({ origin: true, credentials: true }));
+
+const authorizeHandler: RequestHandler = (req, res) => {
+    axios.get('https://api.ecobee.com/authorize', {
+        params: {
+            response_type: 'ecobeePin',
+            client_id: req.query.apikey,
+            scope: 'smartWrite'
+        }
+    }).then((resp) => {
+        res.json(resp.data);
+    }).catch((error) => {
+        console.error(error);
+    });
 };
-app.get('/api', apiHandler);
+app.get('/api/authorize', authorizeHandler);
 
 app.use(express.static(path.join(__dirname, './www')));
 
