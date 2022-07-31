20e0faa9664ec2c7ec62ef56ee71c972de1cb4ed
Drakmyth <shaun.hamman@gmail.com>
2022-07-30 22:13:39 -0700

    Implement ability to request PIN

--- frontend/src/components/EcobeeConfiguration.vue
+++ frontend/src/components/EcobeeConfiguration.vue
@@ -1,21 +1,39 @@
-<script setup lang="ts">
-</script>
-
 <template>
     <div class="ecobee-config">
         <h2>Ecobee Configuration</h2>
         <form>
             <label for="ecobee-apikey">API Key:</label>
-            <input type="text" id="ecobee-apikey" name="ecobee-apikey" /><br />
-            <button>Request PIN</button><br />
-            <span>PIN:</span>
-            <span>ABC123</span><br />
+            <input type="text" id="ecobee-apikey" name="ecobee-apikey" v-model="apikey" /><br />
+            <button @click.prevent=requestPin>Request PIN</button><br />
+            <span>PIN: {{pin}}</span><br />
             <span>Time Remaining:</span>
             <span>9:32</span><br />
             <button>Complete Authorization</button>
         </form>
     </div>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+
+const apikey = ref('');
+const pin = ref('');
+
+const requestPin = () => {
+    axios.get('http://localhost:3001/api/authorize', {
+        params: {
+            apikey: apikey.value,
+        }
+    }).then((resp) => {
+        console.log(resp.data);
+        pin.value = resp.data.ecobeePin;
+    }).catch((error) => {
+        console.error(error);
+    })
+};
+
+</script>
 
 <style>
 </style>
