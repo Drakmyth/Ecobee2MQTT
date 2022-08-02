9fd1b7b4325820b2c534cd859e11cd6aa323a0fb
Drakmyth <shaun.hamman@gmail.com>
2022-08-02 10:55:22 -0700

    Replace axios with node-fetch, use polling to finalize authorization instead of waiting for client request

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,13 @@
 import express, { RequestHandler } from 'express';
 import path from 'node:path';
-import axiosPkg from 'axios';
-const { Axios } = axiosPkg;
 import cors from 'cors';
 import yaml from 'js-yaml';
 import fs from 'node:fs/promises';
 import { E2MSettings } from './config.js';
 import { Server } from 'socket.io';
 import http from 'node:http';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
 
 const app = express();
 const server = http.createServer(app);
@@ -18,12 +18,11 @@ const io = new Server(server, {
     }
 });
 
-const axios = new Axios();
-
 app.use(cors({ origin: true, credentials: true }));
 
 const settingsFilePath = './config/settings.yaml';
 let settings = E2MSettings.getDefaults();
+let authorizeInterval: NodeJS.Timer;
 
 await fs.readFile(settingsFilePath, 'utf-8')
     .then(data => {
@@ -47,46 +46,105 @@ await fs.readFile(settingsFilePath, 'utf-8')
 
 globalThis.CONFIG = settings;
 
-const authorizeHandler: RequestHandler = (req, res) => {
-    axios.get('https://api.ecobee.com/authorize', {
-        params: {
-            response_type: 'ecobeePin',
-            client_id: req.query.apikey,
-            scope: 'smartWrite'
-        }
-    }).then((resp) => {
-        res.json(resp.data);
-    }).catch((error) => {
-        console.error(error);
-    });
-};
+interface AuthorizeRequest {
+    appkey: string;
+    scope: string;
+}
 
-const tokenHandler: RequestHandler = (req, res) => {
-    axios.post('https://api.ecobee.com/token', {}, {
-        params: {
-            grant_type: 'ecobeePin',
-            code: '<AUTHCODE>',
-            client_id: '<APPKEY>',
-            ecobee_type: 'jwt'
-        }
-    }).then(resp => {
-        console.log(resp);
-    }).catch(error => {
+class AuthorizeResponse {
+    constructor(pin: string, expires_in: number) { }
+}
+
+interface EcobeeAuthorizeResponse {
+    ecobeePin: string,
+    expires_in: number,
+    code: string,
+    scope: string,
+    interval: number;
+}
+
+interface EcobeeTokenResponse {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    refresh_token: string,
+    scope: string;
+}
+
+interface EcobeeAuthError {
+    error: string,
+    error_description: string,
+    error_uri: string;
+}
+
+class EcobeeTokenRequestQuery {
+    constructor(public grant_type: string, public code: string, public client_id: string, public ecobee_type: string) { }
+}
+
+type AuthErrors = 'access_denied' | 'invalid_request' | 'invalid_client' | 'invalid_grant' | 'unauthorized_client' | 'unsupported_grant_type' | 'invalid_scope' | 'not_supported' | 'account_locked' | 'account_disabled' | 'authorization_pending' | 'authorization_expired' | 'slow_down';
+
+const authorizeHandler: RequestHandler = async (req, res) => {
+    console.log('\nInitiate Authorization Process...');
+    const body = req.body as AuthorizeRequest;
+    const params = new URLSearchParams({
+        response_type: 'ecobeePin',
+        client_id: body.appkey,
+        scope: body.scope
+    });
+    console.log(params);
+
+    const response = await fetch('https://api.ecobee.com/authorize?' + params);
+    if (response.status >= 300) {
+        const error = await response.json() as EcobeeAuthError;
+        console.log('Error Initializing Authorization');
         console.log(error);
-    });
+        res.json(error);
+        return;
+    }
+
+    console.log('Authorization Initiated');
+    const data = await response.json() as EcobeeAuthorizeResponse;
+    console.log(data);
+    authorizeInterval = setInterval(async () => {
+        await tryGetToken(data.code, body.appkey);
+    }, data.interval * 1000);
+    res.json(new AuthorizeResponse(data.ecobeePin, data.expires_in));
 };
 
-app.get('/api/authorize', authorizeHandler);
-app.get('/api/token', tokenHandler);
+const tryGetToken = async (authcode: string, appkey: string) => {
+    console.log(`\nVerifying Authorization: ${authcode}...`);
+    const params = new URLSearchParams({
+        grant_type: 'ecobeePin',
+        code: authcode,
+        client_id: appkey,
+        ecobee_type: 'jwt'
+    });
 
+    const response = await fetch('https://api.ecobee.com/token?' + params, { method: 'POST' });
+    if (response.status >= 300) {
+        const error = await response.json() as EcobeeAuthError;
+        console.log('Authorization Not Complete');
+        console.log(error);
+        if (error.error as AuthErrors !== 'authorization_pending') {
+            clearInterval(authorizeInterval);
+        }
+        return;
+    }
+
+    console.log('Authorization Verified');
+    const data = await response.json() as EcobeeTokenResponse;
+    console.log(data);
+    // TODO: persist token to file
+    io.emit('authorized', 'success');
+    clearInterval(authorizeInterval);
+};
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(path.dirname('./www'))));
+app.post('/api/authorize', authorizeHandler);
 
 io.on('connection', client => {
     console.log('client connected');
-    client.emit('hello', 'Hello from server!');
-    client.on('event', data => {
-        console.log(data);
-    });
     client.on('disconnect', () => {
         console.log('client disconnected');
     });
