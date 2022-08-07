c611f1d8397ec2ba6e117aab816615b9a0e17d56
Drakmyth <shaun.hamman@gmail.com>
2022-08-07 15:59:37 -0700

    Fix server unit test configuration not handling ESM properly

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,11 +1,22 @@
 import type { Config } from '@jest/types';
+import { default as jest } from 'jest';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            useESM: true
+        }
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
     collectCoverageFrom: [
         'src/**/*.ts',
         '!src/server.ts'
-    ]
+    ],
+    extensionsToTreatAsEsm: ['.ts'],
+    silent: true
 };
 export default config;
