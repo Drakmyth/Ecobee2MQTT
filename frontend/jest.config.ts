b59a3bb462e0fe4b277a1b6080690011c98bd4b4
Drakmyth <shaun.hamman@gmail.com>
2022-08-09 20:39:42 -0700

    Update frontend to use ESM and refresh test snapshots

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,23 +1,34 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'jsdom',
     testEnvironmentOptions: {
         customExportConditions: ["node", "node-addons"],
+    },
+    globals: {
+        'ts-jest': {
+            useESM: true,
+            diagnostics: {
+                ignoreCodes: ['TS151001'],
+            },
+        }
     },
     transform: {
         '^.+\\.vue$': '@vue/vue3-jest',
     },
     moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
         '^@/(.*)$': '<rootDir>/src/$1'
     },
     moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue'],
+    extensionsToTreatAsEsm: ['.ts'],
     collectCoverageFrom: [
         'src/**/*.{ts,vue}',
         '!src/shims-vue.d.ts',
         '!src/main.ts',
         '!src/router/index.ts' // makes coverage crash, see https://github.com/facebook/jest/issues/13007
-    ]
+    ],
+    silent: true
 };
 export default config;
